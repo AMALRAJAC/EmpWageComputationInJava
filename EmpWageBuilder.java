73cba84c064615e9feb250af74b75d358231d069
unknown <acamalraj@gmail.com>
2021-03-30 20:20:04 +0530

    add employee wage for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,40 +3,40 @@ public class EmpWageBuilder {
 	
 	
 	private static int empRatePerHours=20;
-	private static int isFullTime=1;
-	private static int isPartTime=0;
 	private static int salary=0;
+	private static int totalWorkingDays=20;
+	static int totalSalary=0;
 	
 	public static void emp(double x) {
-		
 		int z=(int) x;
 		//condition check employee fulltime or parttime
 	switch (z) {
 	case 1 :
-		System.out.println("employee is fulltime");
 		int empWorkingHour=8;
 		salary=(empWorkingHour*empRatePerHours);
+		totalSalary = totalSalary +salary;
 		break;
 	case 0 :
-		System.out.println("employee is parttime");
 		int empWorkingHour1=4;
 		salary=(empWorkingHour1*empRatePerHours);
+		totalSalary = totalSalary +salary;
 		break;
 		
 	default :
 	    System.out.println("error");
 	    break;
 	}
-		System.out.println("salary = "+salary);
+	
 	}
 
 	public static void main ( String[] args ) {
-		
+		for(int day=0; day< totalWorkingDays;day++) {
 		// take random numbers 1 or 0
 		double empCheck = Math.floor(Math.random()*10)%2 ;
-		
 		//method call
 		emp(empCheck);
+		}
+		System.out.println("total salary for a month = "+totalSalary);
 	}
 	
 	
