96607cbcf44bdd8fe08e87378f30d5e769c4d4be
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-01 10:19:38 +0530

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,23 +1,17 @@
 
 public class EmpWageBuilder {
 	
-	public static void emp(double x) {
-		//condition check employee present or not
-		if (x==1) {
-			System.out.println("employee is present");
-		}else {
-			System.out.println("employee is absent");
-		}
-	}
+	 private static final int isPresent=1;
 
 	public static void main ( String[] args ) {
-		
-		// take random numbers 1 or 0
-		double empCheck = Math.floor(Math.random()*10)%2 ;
-		
-		//method call
-		emp(empCheck);
+
+				int empCheck =(int) Math.floor(Math.random()*10)%2 ;
+	
+						if( isPresent ==empCheck) {
+							System.out.println("employee is present");
+						}
+						else {
+							System.out.println("employee is absent");
+						}
 	}
-	
-	
 }
