43b642e65d25dc1f0f97d71e60a2788051340de4
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-01 10:01:27 +0530

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,41 +3,37 @@ public class EmpWageBuilder {
 	
 	
 	private static int empRatePerHours=20;
-	private static int salary=0;
-	private static int totalWorkingDays=20;
+	static int totalWorkingDays=20;
+	static int empWorkingHour=0;
 	static int totalSalary=0;
-	
-	public static void emp(double x) {
-		int z=(int) x;
-		//condition check employee fulltime or parttime
-	switch (z) {
-	case 1 :
-		int empWorkingHour=8;
-		salary=(empWorkingHour*empRatePerHours);
-		totalSalary = totalSalary +salary;
-		break;
-	case 0 :
-		int empWorkingHour1=4;
-		salary=(empWorkingHour1*empRatePerHours);
-		totalSalary = totalSalary +salary;
-		break;
-		
-	default :
-	    System.out.println("error");
-	    break;
-	}
-	
-	}
+	static int salary=0;
+	 private static final int fullTime=2;
+	 private static final int partTime=1;
 
 	public static void main ( String[] args ) {
-		for(int day=0; day< totalWorkingDays;day++) {
-		// take random numbers 1 or 0
-		double empCheck = Math.floor(Math.random()*10)%2 ;
-		//method call
-		emp(empCheck);
+
+		for (int day=0; day< totalWorkingDays;day++) {
+				int empCheck =(int) Math.floor(Math.random()*10)%3 ;
+		
+						switch (empCheck) {
+						case fullTime :
+							empWorkingHour=8;
+							
+							break;
+						case partTime :
+							empWorkingHour=4;
+							
+							break;
+							
+						default :
+						    empWorkingHour=0;
+						    break;
+						}
+	
+						salary=empWorkingHour*empRatePerHours;
+						totalSalary = totalSalary +salary;
+						
 		}
 		System.out.println("total salary for a month = "+totalSalary);
 	}
-	
-	
 }
