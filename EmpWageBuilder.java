67b5fae34fcc1fd70656e4dab85fa8583b05293b
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-01 09:46:39 +0530

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,50 +1,46 @@
-
 public class EmpWageBuilder {
 	
 	
 	private static int empRatePerHours=20;
-	private static int salary=0;
 	static int totalWorkingDays=0;
 	static int totalSalary=0;
 	static int totalEmpHours=0;
 	private static int numWorkingDays=20;
-	private static int maxHoursInMonth=10;
-	
-	public static void emp(double x) {
-		int z=(int) x;
-		//condition check employee fulltime or parttime
-	switch (z) {
-	case 1 :
-		int empWorkingHour=8;
-		salary=(empWorkingHour*empRatePerHours);
-		totalSalary = totalSalary +salary;
-		totalEmpHours=totalEmpHours+empWorkingHour;
-		break;
-	case 0 :
-		int empWorkingHour1=4;
-		salary=(empWorkingHour1*empRatePerHours);
-		totalSalary = totalSalary +salary;
-		totalEmpHours=totalEmpHours+empWorkingHour1;
-		break;
-		
-	default :
-	    System.out.println("error");
-	    break;
-	}
-	
-	}
+	private static int maxHoursInMonth=100;
+	private static int empWorkingHour=0;
+	 private static final int fullTime=1;
+	 private static final int partTime=0;
 
 	public static void main ( String[] args ) {
 
 		while (totalEmpHours<maxHoursInMonth && totalWorkingDays<numWorkingDays) {
-			totalEmpHours++;
-		// take random numbers 1 or 0
-		double empCheck = Math.floor(Math.random()*10)%2 ;
-		//method call
-		emp(empCheck);
+				totalWorkingDays++;
+				int empCheck =(int) Math.floor(Math.random()*10)%3 ;
+		
+		
+	
+						switch (empCheck) {
+						case fullTime :
+							empWorkingHour=8;
+							
+							break;
+						case partTime :
+							empWorkingHour=4;
+							
+							break;
+							
+						default :
+						    empWorkingHour=0;
+						    break;
+						}
+						
+	
+	
+						totalEmpHours=totalEmpHours+empWorkingHour;
 		}
+	
+		totalSalary = totalEmpHours *empRatePerHours;
 		System.out.println("total salary for a month = "+totalSalary);
+	
 	}
-	
-	
 }
